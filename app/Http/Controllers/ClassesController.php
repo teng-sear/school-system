0dcfd019b180a5e474d8d820c55cc86171f913ed
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-22 18:10:13 +0700

    Menambah controller edit by id dan update untuk class

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -34,4 +34,19 @@ class ClassesController extends Controller
 
         return view('admin.class.class_list', $data);
     }
+
+    public function edit($id)
+    {
+        $data['class'] = Classes::find($id);
+        return view('admin.class.class_edit', $data);
+    }
+
+    public function update(Request $request)
+    {
+        $data = Classes::find($request->id);
+        $data->name = $request->name;
+        $data->update();
+
+        return redirect()->route('class.read')->with('success', 'Class Updated Successfully');
+    }
 }
