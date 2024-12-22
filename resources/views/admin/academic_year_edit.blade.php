20f7b9c4aac0ef46db7e658f19af23eabb54f9ac
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-22 15:14:14 +0700

    Membuat view form edit academic year dan menjalanlan fungsinya

---
+++ resources/views/admin/academic_year_edit.blade.php
@@ -0,0 +1,69 @@
+@extends('admin.layout')
+
+@section('content')
+    {{-- content form --}}
+    <div class="content-wrapper">
+
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Academic Year</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Academic Year</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary">
+
+                            @if (Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{ Session::get('success') }}
+                                </div>
+                            @endif
+
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Academic Year</h3>
+                            </div>
+
+                            {{-- metode default HTML hanya mendukung GET dan POST --}}
+                            <form action="{{ route('academic-year.update') }}" method="post">
+
+                                @csrf
+                                @method('PUT') <!-- Mengubah metode menjadi PUT -->
+
+                                <input type="hidden" name="id" value="{{ $academic_year->id }}">
+
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Academic Year</label>
+                                        <input type="date" name="name" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Enter Academic Year"
+                                            value="{{ old('name', $academic_year->name) }}">
+                                    </div>
+                                    @error('name')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Update Academic Year</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
