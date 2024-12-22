42380df2dabb3280dd9347fd0bfdf33342a50dc8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-22 18:10:37 +0700

    Membuat view form edit dan menjalankan fungsinya

---
+++ resources/views/admin/class/class_edit.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layout')
+
+@section('content')
+    {{-- content form --}}
+    <div class="content-wrapper">
+
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Class</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Class Management</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary">
+
+                            @if (Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{ Session::get('success') }}
+                                </div>
+                            @endif
+
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Class</h3>
+                            </div>
+
+                            {{-- metode default HTML hanya mendukung GET dan POST --}}
+                            <form action="{{ route('class.update') }}" method="post">
+
+                                @csrf
+                                @method('PUT') <!-- Mengubah metode menjadi PUT -->
+
+                                <input type="hidden" name="id" value="{{ $class->id }}">
+
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputName">Class</label>
+                                        <input type="text" name="name" class="form-control" id="exampleInputName"
+                                            placeholder="Enter Class Name" value="{{ old('name', $class->name) }}">
+                                    </div>
+                                    @error('name')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Update Class</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
