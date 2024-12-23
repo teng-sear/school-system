867e538fe315dec3bfd0ded340f947d34b65b14f
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-23 11:04:32 +0700

    Membuat model dan migrasi table fee structure database

---
+++ app/Models/FeeStructure.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeeStructure extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'class_id',
+        'academic_year_id',
+        'fee_head_id',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december',
+        'january',
+        'february',
+        'march',
+    ];
+}
