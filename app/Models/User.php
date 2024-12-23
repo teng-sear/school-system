6771de8b55ebf9ff1718dde1f1630ad83b866c25
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-23 21:40:18 +0700

    Menambah function studentClass dam studentAcademicYear pada user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function studentClass()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function studentAcademicYear()
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year_id');
+    }
 }
