646c88e880ac4398fdd03c832d248da0e6795d27
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-23 07:07:51 +0700

    Menambah menu fee pada sidebar

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -268,6 +268,31 @@
                                 </li>
                             </ul>
                         </li>
+
+                        {{-- fee --}}
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Fee
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ route('fee-head.create') }}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Add Fee</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('class.read') }}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>View Fee</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </nav>
             </div>
