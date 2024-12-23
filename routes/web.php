db275d0e24a40527dab1d64e38aecad1206a8f2c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-23 21:42:24 +0700

    Menambah route get untuk list table student

--- routes/web.php
+++ routes/web.php
@@ -61,5 +61,6 @@ Route::group(['prefix' => 'admin'], function () {
         // fee structure management
         Route::get('student/create', [StudentController::class, 'index'])->name('student.create');
         Route::post('student/store', [StudentController::class, 'store'])->name('student.store');
+        Route::get('student/read', [StudentController::class, 'read'])->name('student.read');
     });
 });
