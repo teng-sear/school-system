49c1a37fe97777b24400549fabf7c7ad4cff2eef
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-24 10:26:26 +0700

    Membuat controller index dan store announcement

---
+++ app/Http/Controllers/AnnouncementController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Announcement;
+use Illuminate\Http\Request;
+
+class AnnouncementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.announcement.form');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'message' => 'required',
+            'type' => 'required'
+        ]);
+        $announcement = new Announcement();
+        $announcement->message = $request->message;
+        $announcement->type = $request->type;
+        $announcement->save();
+
+        // redirec ke halaman dengan pesan sukses
+        return redirect()->route('announcement.create')->with(
+            'success',
+            'Announcement Added Successfully'
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Announcement $announcement)
+    {
+        //
+    }
+}
