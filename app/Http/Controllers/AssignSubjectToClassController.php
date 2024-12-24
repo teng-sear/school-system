45f4183b22b3d3b18c16577d5c271200f317449e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-24 20:50:06 +0700

    Menambah controller read assign

--- app/Http/Controllers/AssignSubjectToClassController.php
+++ app/Http/Controllers/AssignSubjectToClassController.php
@@ -9,9 +9,6 @@ use Illuminate\Http\Request;
 
 class AssignSubjectToClassController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $data['classes'] = Classes::all();
@@ -19,17 +16,6 @@ class AssignSubjectToClassController extends Controller
         return view('admin.assign_subject.form', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -54,35 +40,9 @@ class AssignSubjectToClassController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(AssignSubjectToClass $assignSubjectToClass)
+    public function read()
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(AssignSubjectToClass $assignSubjectToClass)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, AssignSubjectToClass $assignSubjectToClass)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(AssignSubjectToClass $assignSubjectToClass)
-    {
-        //
+        $data['assign_subjects'] = AssignSubjectToClass::all();
+        return view('admin.assign_subject.table', $data);
     }
 }
