9ec356bb7da1715966a5a05e1fa544dce7c4d2b3
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-24 19:10:31 +0700

    Membuat model assign subject to class dan malakukan migrasi database

---
+++ app/Models/AssignSubjectToClass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AssignSubjectToClass extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'class_id',
+        'subject_id'
+    ];
+}
