74c134a392eb220d9cd392fc88c288a0fdaf4c60
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-24 08:22:38 +0700

    Menambah controller change password student

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -35,5 +37,31 @@ class UserController extends Controller
     {
         Auth::logout();
         return redirect()->route('student.login')->with('error', 'Logout successfully');
+    }
+
+    public function changePassword()
+    {
+        return view('student.change_password');
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required',
+            'confirm_password' => 'required|same:new_password'
+        ]);
+        $old_password = $request->old_password;
+        $new_password = $request->new_password;
+        // $confirm_password = $request->confirm_password;
+        $user = User::find(Auth::user()->id); // find user
+
+        if (Hash::check($old_password, $user->password)) {
+            $user->password = Hash::make($new_password);
+            $user->save();
+            return redirect()->back()->with('success', 'Password updated successfully');
+        } else {
+            return redirect()->back()->withErrors(['old_password' => 'Old password is incorrect']);
+        }
     }
 }
