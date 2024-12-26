21dceafe40890d1714626197d039cfa4cd1c1a6c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-26 20:24:27 +0700

    Membuat controller index dan store untuk timetable

---
+++ app/Http/Controllers/TimetableController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classes;
+use App\Models\Day;
+use App\Models\Subject;
+use App\Models\Timetable;
+use Illuminate\Http\Request;
+
+class TimetableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data['days'] = Day::all();
+        $data['classes'] = Classes::all();
+        $data['subjects'] = Subject::all();
+        return view('admin.timetable.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $class_id = $request->class_id;
+        $subject_id = $request->subject_id;
+        foreach ($request->timetable as $timetable) {
+            $day_id = $timetable['day_id'];
+            $start_time = $timetable['start_time'];
+            $end_time = $timetable['end_time'];
+            $room_no = $timetable['room_no'];
+
+            if ($start_time != null) {
+                Timetable::updateOrCreate(
+                    [
+                        'class_id' => $class_id,
+                        'subject_id' => $subject_id,
+                        'day_id' => $day_id,
+                    ],
+                    [
+                        'class_id' => $class_id,
+                        'subject_id' => $subject_id,
+                        'day_id' => $day_id,
+                        'start_time' => $start_time,
+                        'end_time' => $end_time,
+                        'room_no' => $room_no,
+                    ]
+                );
+            }
+        }
+
+        return redirect()->route('timetable.create')->with('success', 'Time Table added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Timetable $timetable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Timetable $timetable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Timetable $timetable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Timetable $timetable)
+    {
+        //
+    }
+}
