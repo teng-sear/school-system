7848aff15f5da97672a4333850eef65b83b062ac
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-26 20:42:19 +0700

    Menambah function yang dibutuhkan untuk timetable model

--- app/Models/Timetable.php
+++ app/Models/Timetable.php
@@ -16,4 +16,19 @@ class Timetable extends Model
         'end_time',
         'room_no'
     ];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class, 'subject_id');
+    }
+
+    public function day()
+    {
+        return $this->belongsTo(Day::class, 'day_id');
+    }
 }
