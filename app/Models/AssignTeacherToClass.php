ac568c9af844a34a84bbd18cb3c43412d66e87bd
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-26 08:59:54 +0700

    Membuat model table assign teacher to class

--- app/Models/AssignTeacherToClass.php
+++ app/Models/AssignTeacherToClass.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AssignTeacherToClass extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'class_id',
+        'subject_id',
+        'teacher_id'
+    ];
 }
