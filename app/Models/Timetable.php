c55d820cf623a9d4f9643efaffc1fb5029e9e648
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-26 20:24:02 +0700

    Membuat model timetable dan migrate table ke database

--- app/Models/Timetable.php
+++ app/Models/Timetable.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Timetable extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'class_id',
+        'subject_id',
+        'day_id',
+        'start_time',
+        'end_time',
+        'room_no'
+    ];
 }
---
+++ database/migrations/2024_12_26_104535_alter_timetables_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('timetables', function (Blueprint $table) {
+            $table->foreignId('class_id')->constrained()->onDelete('cascade')->nullable()->change();
+            $table->foreignId('day_id')->constrained()->onDelete('cascade')->nullable()->change();
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade')->nullable()->change();
+            $table->string('start_time')->nullable()->change();
+            $table->string('end_time')->nullable()->change();
+            $table->string('room_no')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('timetables', function (Blueprint $table) {
+            $table->foreignId('class_id')->constrained()->onDelete('cascade')->change();
+            $table->foreignId('day_id')->constrained()->onDelete('cascade')->change();
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade')->change();
+            $table->string('start_time')->change();
+            $table->string('end_time')->change();
+            $table->string('room_no')->change();
+        });
+    }
+};
