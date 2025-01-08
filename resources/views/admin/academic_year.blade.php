8cff78509462219f8ec1f16c9feee6a911729f03
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-08 21:51:51 +0700

    Menambah button back pada add academic year

--- resources/views/admin/academic_year.blade.php
+++ resources/views/admin/academic_year.blade.php
@@ -12,8 +12,12 @@
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                            <li class="breadcrumb-item active">Academic Year</li>
+                            <a href="{{ route('academic-year.read') }}" style="display: inline;">
+                                <button type="submit" class="btn btn-secondary text-sm">
+                                    <i class="nav-icon fas fa-solid fa-arrow-left text-xs"></i>
+                                    Back
+                                </button>
+                            </a>
                         </ol>
                     </div>
                 </div>
