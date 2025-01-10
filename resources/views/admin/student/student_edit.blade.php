8b1ff6df48c14e670c05f76d11281207920757c9
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-10 14:29:14 +0700

    Menambah button back pada edit student

--- resources/views/admin/student/student_edit.blade.php
+++ resources/views/admin/student/student_edit.blade.php
@@ -8,12 +8,16 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Student</h1>
+                        <h1>Siswa</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                            <li class="breadcrumb-item active">Student</li>
+                            <a href="{{ route('student.read') }}" style="display: inline;">
+                                <button type="submit" class="btn btn-secondary text-sm">
+                                    <i class="nav-icon fas fa-solid fa-arrow-left text-xs"></i>
+                                    Back
+                                </button>
+                            </a>
                         </ol>
                     </div>
                 </div>
@@ -33,7 +37,7 @@
                             @endif
 
                             <div class="card-header">
-                                <h3 class="card-title">Edit Student</h3>
+                                <h3 class="card-title">Edit Siswa</h3>
                             </div>
 
                             {{-- metode default HTML hanya mendukung GET dan POST --}}
@@ -48,7 +52,7 @@
                                     <div class="row">
                                         {{-- select academic_years --}}
                                         <div class="form-group col-md-4">
-                                            <label>Select Academic Years</label>
+                                            <label>Pilih Tahun Akademik</label>
                                             <select name="academic_year_id" class="form-control">
                                                 <option value="">Select Academic Years</option>
                                                 @foreach ($academic_years as $academic_year)
@@ -65,7 +69,7 @@
 
                                         {{-- select class --}}
                                         <div class="form-group col-md-4">
-                                            <label>Select Class</label>
+                                            <label>Pilih Kelas</label>
                                             <select name="class_id" class="form-control">
                                                 <option value="">Select Class</option>
                                                 @foreach ($classes as $class)
@@ -81,7 +85,7 @@
 
                                         {{-- addmoison date --}}
                                         <div class="form-group col-md-4">
-                                            <label>Admission Date</label>
+                                            <label>Tanggal Penerimaan</label>
                                             <input type="date" name="admission_date" class="form-control"
                                                 value="{{ old('admission_date', $student->admission_date) }}">
                                             @error('admission_date')
@@ -93,9 +97,9 @@
                                     <div class="row">
                                         {{-- student name --}}
                                         <div class="form-group col-md-4">
-                                            <label for="exampleInput">Student Name</label>
+                                            <label for="exampleInput">Nama Siswa</label>
                                             <input type="text" name="name" class="form-control" id="exampleInput"
-                                                placeholder="Enter Student name" value="{{ old('name', $student->name) }}">
+                                                placeholder="Enter nama siswa" value="{{ old('name', $student->name) }}">
                                             @error('name')
                                                 <p class="text-danger">{{ $message }}</p>
                                             @enderror
@@ -103,9 +107,9 @@
 
                                         {{-- student's father name --}}
                                         <div class="form-group col-md-4">
-                                            <label for="exampleInput">Student's Father Name</label>
+                                            <label for="exampleInput">Nama Ayah Siswa</label>
                                             <input type="text" name="father_name" class="form-control" id="exampleInput"
-                                                placeholder="Enter Student Father name"
+                                                placeholder="Enter nama Ayah siswa"
                                                 value="{{ old('father_name', $student->father_name) }}">
                                             @error('father_name')
                                                 <p class="text-danger">{{ $message }}</p>
@@ -114,9 +118,9 @@
 
                                         {{-- student's mother name --}}
                                         <div class="form-group col-md-4">
-                                            <label for="exampleInput">Student's Mother Name</label>
+                                            <label for="exampleInput">Nama Ibu Siswa</label>
                                             <input type="text" name="mother_name" class="form-control" id="exampleInput"
-                                                placeholder="Enter Student Mother name"
+                                                placeholder="Enter nama Ibu siswa"
                                                 value="{{ old('mother_name', $student->mother_name) }}">
                                             @error('mother_name')
                                                 <p class="text-danger">{{ $message }}</p>
@@ -127,7 +131,7 @@
                                     <div class="row">
                                         {{-- dob --}}
                                         <div class="form-group col-md-6">
-                                            <label for="exampleInput">Date Of Birth</label>
+                                            <label for="exampleInput">Tanggal Lahir</label>
                                             <input type="date" name="dob" class="form-control" id="exampleInput"
                                                 value="{{ old('dob', $student->dob) }}">
                                             @error('dob')
@@ -137,9 +141,9 @@
 
                                         {{-- mobno --}}
                                         <div class="form-group col-md-6">
-                                            <label for="exampleInput">Mobile Number</label>
+                                            <label for="exampleInput">No Telepon</label>
                                             <input type="text" name="mobno" class="form-control" id="exampleInput"
-                                                placeholder="Enter Mobile Number"
+                                                placeholder="Enter Nomor Telepon"
                                                 value="{{ old('mobno', $student->mobno) }}">
                                             @error('mobno')
                                                 <p class="text-danger">{{ $message }}</p>
@@ -150,9 +154,9 @@
                                     <div class="row">
                                         {{-- email addreess --}}
                                         <div class="form-group col-md-6">
-                                            <label for="exampleInput">Email Address</label>
+                                            <label for="exampleInput">Alamat Email</label>
                                             <input type="text" name="email" class="form-control" id="exampleInput"
-                                                placeholder="Enter Email Address"
+                                                placeholder="Enter Alamat Email"
                                                 value="{{ old('email', $student->email) }}">
                                             @error('email')
                                                 <p class="text-danger">{{ $message }}</p>
@@ -162,7 +166,7 @@
                                 </div>
 
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Update Student</button>
+                                    <button type="submit" class="btn btn-primary text-sm">Update</button>
                                 </div>
                             </form>
                         </div>
