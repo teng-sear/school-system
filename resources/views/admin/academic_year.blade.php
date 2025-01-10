9d5de46728c13cc46323ddb66089cdc8c3f9a80f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-10 08:56:08 +0700

    Merubah nama page add academik year

--- resources/views/admin/academic_year.blade.php
+++ resources/views/admin/academic_year.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Academic Year</h1>
+                        <h1>Tahun Akademik</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -37,7 +37,7 @@
                             @endif
 
                             <div class="card-header">
-                                <h3 class="card-title">Add Academic Year</h3>
+                                <h3 class="card-title">Add Tahun Akademik</h3>
                             </div>
 
                             <form action="{{ route('academic-year.store') }}" method="post">
@@ -46,7 +46,7 @@
 
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Academic Year</label>
+                                        <label for="exampleInputEmail1">Tahun Akademik</label>
                                         <input type="date" name="name" class="form-control" id="exampleInputEmail1"
                                             placeholder="Enter Academic Year">
                                     </div>
