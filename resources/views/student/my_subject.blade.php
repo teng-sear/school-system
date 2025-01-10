553e1e26abde9ea530516168177ea376f9b2bd96
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-10 20:41:29 +0700

    Merubah nama title page dan kolom table my subject

--- resources/views/student/my_subject.blade.php
+++ resources/views/student/my_subject.blade.php
@@ -12,14 +12,9 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>My Subject & Teacher</h1>
+                        <h1>Mata Pelajaran & Guru Saya</h1>
                     </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('student.dashboard') }}">Home</a></li>
-                            <li class="breadcrumb-item active">My Subject & Teacher List</li>
-                        </ol>
-                    </div>
+
                 </div>
             </div>
         </section>
@@ -43,9 +38,9 @@
                                     {{-- table header --}}
                                     <thead>
                                         <tr>
-                                            <th>ID</th>
-                                            <th>Subject Name</th>
-                                            <th>Teacher Name</th>
+                                            <th>No.</th>
+                                            <th>Nama Pelajaran</th>
+                                            <th>Nama Guru</th>
                                             <th>Theory/Practical</th>
                                         </tr>
                                     </thead>
@@ -54,13 +49,12 @@
                                     <tbody>
                                         @foreach ($my_subjects as $item)
                                             <tr>
-                                                <td>{{ $item->id }}</td>
+                                                <td>{{ $loop->iteration }}.</td> <!-- Nomor Urut -->
                                                 <td>{{ $item->subject->name }}</td>
                                                 <td>{{ $item->teacher->name }}</td>
                                                 <td>{{ $item->subject->type }}</td>
                                             </tr>
                                         @endforeach
-
                                     </tbody>
                                 </table>
                             </div>
