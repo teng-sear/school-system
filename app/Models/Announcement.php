cacc7e8bf762e453aa425a1e8e76be3e5282811d
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-11 09:01:46 +0700

    Modifikasi model announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    //
+    // use HasFactory;
+    // protected $fillable = [
+    //     'announcement_id',
+    //     'type'
+    // ];
 }
