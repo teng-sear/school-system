fa877fcf49871a67625aabe4c1512e2261f7110a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-17 21:30:29 +0700

    Memperbaiki semua penulisan route agar standar Restful API

--- routes/web.php
+++ routes/web.php
@@ -83,90 +83,90 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('table', [AdminController::class, 'table'])->name('admin.table');
 
         // academic year
+        Route::get('academic-year', [AcademicYearController::class, 'read'])->name('academic-year.read');
         Route::get('academic-year/create', [AcademicYearController::class, 'index'])->name('academic-year.create');
         Route::post('academic-year/store', [AcademicYearController::class, 'store'])->name('academic-year.store');
-        Route::get('academic-year/read', [AcademicYearController::class, 'read'])->name('academic-year.read');
-        Route::delete('academic-year/delete/{id}', [AcademicYearController::class, 'delete'])->name('academic-year.delete');
-        Route::get('academic-year/edit/{id}', [AcademicYearController::class, 'edit'])->name('academic-year.edit');
+        Route::get('academic-year/{id}/edit/', [AcademicYearController::class, 'edit'])->name('academic-year.edit');
         Route::put('academic-year/update', [AcademicYearController::class, 'update'])->name('academic-year.update');
+        Route::delete('academic-year/delete/{id}', [AcademicYearController::class, 'delete'])->name('academic-year.delete');
 
         // announcement management
+        Route::get('announcement', [AnnouncementController::class, 'read'])->name('announcement.read');
         Route::get('announcement/create', [AnnouncementController::class, 'index'])->name('announcement.create');
         Route::post('announcement/store', [AnnouncementController::class, 'store'])->name('announcement.store');
-        Route::get('announcement/read', [AnnouncementController::class, 'read'])->name('announcement.read');
-        Route::get('announcement/edit/{id}', [AnnouncementController::class, 'edit'])->name('announcement.edit');
+        Route::get('announcement/{id}/edit', [AnnouncementController::class, 'edit'])->name('announcement.edit');
         Route::put('announcement/update/{id}', [AnnouncementController::class, 'update'])->name('announcement.update');
         Route::delete('announcement/delete/{id}', [AnnouncementController::class, 'delete'])->name('announcement.delete');
 
         // classes management
+        Route::get('class', [ClassesController::class, 'read'])->name('class.read');
         Route::get('class/create', [ClassesController::class, 'index'])->name('class.create');
         Route::post('class/store', [ClassesController::class, 'store'])->name('class.store');
-        Route::get('class/read', [ClassesController::class, 'read'])->name('class.read');
-        Route::delete('class/delete/{id}', [ClassesController::class, 'delete'])->name('class.delete');
-        Route::get('class/edit/{id}', [ClassesController::class, 'edit'])->name('class.edit');
+        Route::get('class/{id}/edit', [ClassesController::class, 'edit'])->name('class.edit');
         Route::put('class/update', [ClassesController::class, 'update'])->name('class.update');
+        Route::delete('class/delete/{id}', [ClassesController::class, 'delete'])->name('class.delete');
 
         // fee head management
+        Route::get('fee-head', [FeeHeadController::class, 'read'])->name('fee-head.read');
         Route::get('fee-head/create', [FeeHeadController::class, 'index'])->name('fee-head.create');
         Route::post('fee-head/store', [FeeHeadController::class, 'store'])->name('fee-head.store');
-        Route::get('fee-head/read', [FeeHeadController::class, 'read'])->name('fee-head.read');
-        Route::get('fee-head/edit/{id}', [FeeHeadController::class, 'edit'])->name('fee-head.edit');
+        Route::get('fee-head/{id}/edit', [FeeHeadController::class, 'edit'])->name('fee-head.edit');
         Route::put('fee-head/update', [FeeHeadController::class, 'update'])->name('fee-head.update');
         Route::delete('fee-head/delete/{id}', [FeeHeadController::class, 'delete'])->name('fee-head.delete');
 
         // fee structure management
+        Route::get('fee-structure', [FeeStructureController::class, 'read'])->name('fee-structure.read');
         Route::get('fee-structure/create', [FeeStructureController::class, 'index'])->name('fee-structure.create');
         Route::post('fee-structure/store', [FeeStructureController::class, 'store'])->name('fee-structure.store');
-        Route::get('fee-structure/read', [FeeStructureController::class, 'read'])->name('fee-structure.read');
-        Route::get('fee-structure/edit/{id}', [FeeStructureController::class, 'edit'])->name('fee-structure.edit');
+        Route::get('fee-structure/{id}/edit', [FeeStructureController::class, 'edit'])->name('fee-structure.edit');
         Route::put('fee-structure/update/{id}', [FeeStructureController::class, 'update'])->name('fee-structure.update');
         Route::delete('fee-structure/delete/{id}', [FeeStructureController::class, 'delete'])->name('fee-structure.delete');
 
         // student management
+        Route::get('student', [StudentController::class, 'read'])->name('student.read');
         Route::get('student/create', [StudentController::class, 'index'])->name('student.create');
         Route::post('student/store', [StudentController::class, 'store'])->name('student.store');
-        Route::get('student/read', [StudentController::class, 'read'])->name('student.read');
-        Route::get('student/edit/{id}', [StudentController::class, 'edit'])->name('student.edit');
+        Route::get('student/{id}/edit', [StudentController::class, 'edit'])->name('student.edit');
         Route::put('student/update/{id}', [StudentController::class, 'update'])->name('student.update');
         Route::delete('student/delete/{id}', [StudentController::class, 'delete'])->name('student.delete');
 
         // subject
+        Route::get('subject', [SubjectController::class, 'read'])->name('subject.read');
         Route::get('subject/create', [SubjectController::class, 'index'])->name('subject.create');
         Route::post('subject/store', [SubjectController::class, 'store'])->name('subject.store');
-        Route::get('subject/read', [SubjectController::class, 'read'])->name('subject.read');
-        Route::get('subject/edit/{id}', [SubjectController::class, 'edit'])->name('subject.edit');
+        Route::get('subject/{id}/edit', [SubjectController::class, 'edit'])->name('subject.edit');
         Route::put('subject/update/{id}', [SubjectController::class, 'update'])->name('subject.update');
         Route::delete('subject/delete/{id}', [SubjectController::class, 'delete'])->name('subject.delete');
 
         // assign subject to class
+        Route::get('assign-subject', [AssignSubjectToClassController::class, 'read'])->name('assign-subject.read');
         Route::get('assign-subject/create', [AssignSubjectToClassController::class, 'index'])->name('assign-subject.create');
         Route::post('assign-subject/store', [AssignSubjectToClassController::class, 'store'])->name('assign-subject.store');
-        Route::get('assign-subject/read', [AssignSubjectToClassController::class, 'read'])->name('assign-subject.read');
-        Route::get('assign-subject/edit/{id}', [AssignSubjectToClassController::class, 'edit'])->name('assign-subject.edit');
+        Route::get('assign-subject/{id}/edit', [AssignSubjectToClassController::class, 'edit'])->name('assign-subject.edit');
         Route::put('assign-subject/update/{id}', [AssignSubjectToClassController::class, 'update'])->name('assign-subject.update');
         Route::delete('assign-subject/delete/{id}', [AssignSubjectToClassController::class, 'delete'])->name('assign-subject.delete');
 
         // teacher management
+        Route::get('teacher', [TeacherController::class, 'read'])->name('teacher.read');
         Route::get('teacher/create', [TeacherController::class, 'index'])->name('teacher.create');
         Route::post('teacher/store', [TeacherController::class, 'store'])->name('teacher.store');
-        Route::get('teacher/read', [TeacherController::class, 'read'])->name('teacher.read');
-        Route::get('teacher/edit/{id}', [TeacherController::class, 'edit'])->name('teacher.edit');
+        Route::get('teacher/{id}/edit', [TeacherController::class, 'edit'])->name('teacher.edit');
         Route::put('teacher/update/{id}', [TeacherController::class, 'update'])->name('teacher.update');
         Route::delete('teacher/delete/{id}', [TeacherController::class, 'delete'])->name('teacher.delete');
 
         // assign teacher 
+        Route::get('assign-teacher', [AssignTeacherToClassController::class, 'read'])->name('assign-teacher.read');
         Route::get('assign-teacher/create', [AssignTeacherToClassController::class, 'index'])->name('assign-teacher.create');
         Route::post('assign-teacher/store', [AssignTeacherToClassController::class, 'store'])->name('assign-teacher.store');
-        Route::get('assign-teacher/read', [AssignTeacherToClassController::class, 'read'])->name('assign-teacher.read');
-        Route::get('assign-teacher/edit/{id}', [AssignTeacherToClassController::class, 'edit'])->name('assign-teacher.edit');
+        Route::get('assign-teacher/{id}/edit', [AssignTeacherToClassController::class, 'edit'])->name('assign-teacher.edit');
         Route::put('assign-teacher/update/{id}', [AssignTeacherToClassController::class, 'update'])->name('assign-teacher.update');
         Route::delete('assign-teacher/delete/{id}', [AssignTeacherToClassController::class, 'delete'])->name('assign-teacher.delete');
         Route::get('findSubject', [AssignTeacherToClassController::class, 'findSubject'])->name('findSubject');
 
         // time table 
+        Route::get('timetable', [TimetableController::class, 'read'])->name('timetable.read');
         Route::get('timetable/create', [TimetableController::class, 'index'])->name('timetable.create');
         Route::post('timetable/store', [TimetableController::class, 'store'])->name('timetable.store');
-        Route::get('timetable/read', [TimetableController::class, 'read'])->name('timetable.read');
         Route::delete('timetable/delete/{id}', [TimetableController::class, 'delete'])->name('timetable.delete');
     });
 
